dd9cc1dc274fe8c64a52894a4c5caa0827093841
RyanMacMillanSoftware <rmac460@aucklanduni.ac.nz>
2016-08-24 11:00:18 +1200

    Oath

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	// top kek
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Ryan rules, Victor drools");
 	}
 
 }
